c2a82a498458e5511cbf830583fe742931cc567e
Sophawit <mornsophawit@gmail.com>
2026-06-23 12:55:07 +0700

    Fix: axios.js file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api',
   // withCredentials: true, // if using cookies
   headers: {
     'Content-Type': 'application/json',
